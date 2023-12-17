c2ecfe047a387be7a7a14df09a982b56b1ab34a6
alex <109973612+acursin@users.noreply.github.com>
2023-12-17 15:58:44 -0500

    Add player choice function

--- scripts.js
+++ scripts.js
@@ -1,6 +1,37 @@
+let choices = ['Rock', 'Paper', 'Scissors'];
+
 function getComputerChoice() {
-    let choices = ['Rock', 'Paper', 'Scissors'];
-    choice = Math.floor(Math.random() * 3);
-    return choices[choice];
+    computerChoice = Math.floor(Math.random() * 3);
+    return choices[computerChoice];
 }
 
+function capitalize(word) {
+    return word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase();
+}
+
+function getPlayerChoice() {
+    let playerChoice = capitalize(prompt('Rock, Paper, or Scissors?', ''));
+    let isInputValid = choices.includes(playerChoice);
+
+    while(isInputValid === false) {
+        playerChoice = capitalize(prompt('Invalid input. Rock, Paper, or Scissors?', ''));
+        isInputValid = choices.includes(playerChoice);
+    }
+
+    return playerChoice;
+}
+
+let playerSelect = getPlayerChoice();
+
+console.log(playerSelect);
+
+// function playRound(playerSelection, computerSelection) {
+//     playerSelection = capitalize(playerSelection);
+//     let checkTie = true;
+
+//     while(checkTie === true) {
+//         if (playerSelection === computerSelection) {
+//             computerSelection = getComputerChoice()
+//         }
+//     }
+// }
