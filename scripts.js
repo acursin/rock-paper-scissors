694666875798fd38c82460fc398bcf758c358d93
alex <109973612+acursin@users.noreply.github.com>
2023-12-17 16:09:53 -0500

    Make capitalize and choice functions more concise

--- scripts.js
+++ scripts.js
@@ -1,37 +1,26 @@
 let choices = ['Rock', 'Paper', 'Scissors'];
 
+function capitalize(word) {
+    return word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase();
+}
+
 function getComputerChoice() {
     computerChoice = Math.floor(Math.random() * 3);
     return choices[computerChoice];
 }
 
-function capitalize(word) {
-    return word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase();
-}
-
 function getPlayerChoice() {
     let playerChoice = capitalize(prompt('Rock, Paper, or Scissors?', ''));
-    let isInputValid = choices.includes(playerChoice);
 
-    while(isInputValid === false) {
+    while(!choices.includes(playerChoice)) {
         playerChoice = capitalize(prompt('Invalid input. Rock, Paper, or Scissors?', ''));
-        isInputValid = choices.includes(playerChoice);
     }
 
     return playerChoice;
 }
 
-let playerSelect = getPlayerChoice();
+function playRound(playerSelection, computerSelection) {
+    while(checkTie === true) {
 
-console.log(playerSelect);
-
-// function playRound(playerSelection, computerSelection) {
-//     playerSelection = capitalize(playerSelection);
-//     let checkTie = true;
-
-//     while(checkTie === true) {
-//         if (playerSelection === computerSelection) {
-//             computerSelection = getComputerChoice()
-//         }
-//     }
-// }
+    }
+}
