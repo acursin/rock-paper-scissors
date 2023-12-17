3b7569309caa90064660c634e3a0da9e72488290
alex <109973612+acursin@users.noreply.github.com>
2023-12-17 16:27:00 -0500

    Add play round function

--- scripts.js
+++ scripts.js
@@ -1,26 +1,29 @@
 let choices = ['Rock', 'Paper', 'Scissors'];
 
-function capitalize(word) {
-    return word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase();
-}
-
-function getComputerChoice() {
-    computerChoice = Math.floor(Math.random() * 3);
-    return choices[computerChoice];
-}
+function capitalize(word) { return word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase(); }
 
 function getPlayerChoice() {
     let playerChoice = capitalize(prompt('Rock, Paper, or Scissors?', ''));
-
     while(!choices.includes(playerChoice)) {
         playerChoice = capitalize(prompt('Invalid input. Rock, Paper, or Scissors?', ''));
     }
-
     return playerChoice;
 }
 
-function playRound(playerSelection, computerSelection) {
-    while(checkTie === true) {
+function getComputerChoice() { return choices[Math.floor(Math.random() * 3)]; }
 
+function playRound(playerSelection, computerSelection) {
+    while(playerSelection === computerSelection) {
+        console.log(`Tie! You both picked ${playerSelection}`)
+        playerSelection = getPlayerChoice();
+        computerSelection = getComputerChoice();
     }
+
+    if (playerSelection === 'Rock' && computerSelection === 'Scissors' || playerSelection === 'Scissors' && computerSelection === 'Paper' || playerSelection === 'Paper' && computerSelection === 'Rock') {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    }
+
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
 }
+
+console.log(playRound(getPlayerChoice(), getComputerChoice()));
