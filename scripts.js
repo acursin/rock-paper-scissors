fd528758ed5399094beb7c5c4448898906193d50
alex <109973612+acursin@users.noreply.github.com>
2024-07-21 07:57:52 -0400

    Implement printGameWinner function

--- scripts.js
+++ scripts.js
@@ -1,45 +1,3 @@
-function getHumanChoice() {
-    let choice = prompt("Enter choice:", "").toLowerCase();
-
-    while (choice != "rock" && choice != "paper" && choice != "scissors") {
-        choice = prompt("Invalid choice. Try again:", "").toLowerCase()
-    }
-
-    return choice;
-}
-
-function printGameWinner(humanFinalScore, computerFinalScore) {
-    const FINAL_SCORE_MSG = `Final score: Human - ${humanFinalScore}, Computer - ${computerFinalScore}`;
-
-    if (humanFinalScore > computerFinalScore) {
-        console.log(`You win! ${FINAL_SCORE_MSG}`);
-    }
-    else if (humanFinalScore < computerFinalScore) {
-        console.log(`You lose! ${FINAL_SCORE_MSG}`);
-    }
-    else {
-        console.log(`Tie! ${FINAL_SCORE_MSG}`);
-    }
-
-    return;
-}
-
-function playGame(rounds) {
-    let humanScore = 0;
-    let computerScore = 0;
-
-    for (let i = 0; i < rounds; i++) {
-        let winner = playRound(getHumanChoice(), getComputerChoice());
-
-        if (winner === "human") {humanScore++};
-        if (winner === "computer") {computerScore++};
-    }
-
-    printGameWinner(humanScore, computerScore);
-
-    return;
-}
-
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
     button.addEventListener("click", playRound);
@@ -74,34 +32,73 @@ function updateScore(winner) {
         const humanScore = document.querySelector(".humanScore");
         humanScore.dataset.score = +humanScore.dataset.score + 1;
         humanScore.textContent = `Human: ${humanScore.dataset.score}`;
-    } else if (winner === "computer") {
+        return +humanScore.dataset.score;
+    }
+
+    if (winner === "computer") {
         const computerScore = document.querySelector(".computerScore");
         computerScore.dataset.score = +computerScore.dataset.score + 1;
         computerScore.textContent = `Computer: ${computerScore.dataset.score}`
+        return +computerScore.dataset.score;
+    }
+
+    if (winner === "reset") {
+        const humanScore = document.querySelector(".humanScore");
+        humanScore.dataset.score = 0;
+        humanScore.textContent = "Human: 0";
+
+        const computerScore = document.querySelector(".computerScore");
+        computerScore.dataset.score = 0;
+        computerScore.textContent = "Computer: 0";
+
+        return;
+    }
+}
+
+function printGameWinner() {
+    const humanFinalScore = document.querySelector(".humanScore");
+    const computerFinalScore = document.querySelector(".computerScore");
+
+    const FINAL_SCORE_MSG = `Final score: Human - ${humanFinalScore.dataset.score}, Computer - ${computerFinalScore.dataset.score}`;
+
+    if (humanFinalScore > computerFinalScore) {
+        recordResult(`You win! ${FINAL_SCORE_MSG}`);
+    }
+    else {
+        recordResult(`You lose! ${FINAL_SCORE_MSG}`);
     }
 
     return;
 }
+
 
 function playRound(e) {
 
     const humanChoice = e.currentTarget.value;
     const computerChoice =  getComputerChoice();
 
+    let winningScore = 0;
+
     if (humanChoice === computerChoice) {
         recordResult(`Tie! You both picked ${humanChoice}!`);
-        return "tie";
+        return;
     }
+
     if (humanChoice === "rock" && computerChoice === "scissors"
         || humanChoice === "paper" && computerChoice === "rock"
         || humanChoice === "scissors" && computerChoice === "paper"
     ) {
         recordResult(`You win! ${capitalize(humanChoice)} beats ${computerChoice}.`);
-        updateScore("human");
-        return "human";
+        winningScore = updateScore("human");
+    } else {
+        recordResult(`You lose! ${capitalize(computerChoice)} beats ${humanChoice}.`);
+        winningScore = updateScore("computer");
     }
 
-    recordResult(`You lose! ${capitalize(computerChoice)} beats ${humanChoice}.`);
-    updateScore("computer");
-    return "computer";
+    if (winningScore === 5) {
+        printGameWinner();
+        updateScore("reset");
+    }
+
+    return;
 }
